6483d558cbf6e0f09c41bba0877f4aa633666897
benBen <benmbenjamin@gmail.com>
2016-04-01 19:46:06 -0700

    new wow

--- js/first.js
+++ js/first.js
@@ -8,30 +8,31 @@ function makeSpace(){
 
 function enterPlayers(p1, p2){
   return new Promise( function(resolve) {
-    console.log("running")
-    var p1Col = '<div id="p0Col" class="pCol"><h2>' + p1.name + '</h2></div>';
-    var p2Col = '<div id="p1Col" class="pCol"><h2>' + p2.name + '</h2></div>';
+    var p1Col = '<div id="p0Col" class="pCol"><h2>' + p1.username + '</h2></div>';
+    var p2Col = '<div id="p1Col" class="pCol"><h2>' + p2.username + '</h2></div>';
     var pCols = p1Col + p2Col;
     $('#space').append(pCols);
-    players.push(p1, p2);
     resolve(getProfPics(p1,p2));
   })
 }
 
-var players = [],
-    ben = {name:"beners"},
-    drake = {name:"champagnepapi"};
+function Player(username) {
+  this.username = username
+}
+
+var ben = new Player("beners");
+var drake = new Player("champagnepapi");
+var radha = new Player("radha.v");
 
 function getProfPic(player, index) {
   return Promise.resolve( $.ajax({
     method: "GET",
-    url: "https://api.instagram.com/v1/users/search?q="+player.name+"&access_token=1578228172.467ede5.04a6ec58145743cc851a2d64b58d9627",
+    url: "https://api.instagram.com/v1/users/search?q="+player.username+"&access_token=1578228172.467ede5.04a6ec58145743cc851a2d64b58d9627",
     dataType: "jsonp",
     jsonp: "callback",
     jsonpCallback: "jsonpcallback",
   }) ).then(
     function(data) {
-      console.log(data.data[0]);
       player.id = data.data[0].id;
       $('#p'+index+'Col').append('<div id="p'+index+'Data" class="pData"><img src="'+data.data[0].profile_picture+'" /></div>')
     }
@@ -42,10 +43,10 @@ function getProfPic(player, index) {
   )
 }
 
-function getProfPics(names) {
-  return Promise.map(arguments, function(name, i){
-    return getProfPic(name, i)
-  }, {concurrency: 1})
+function getProfPics(players) {
+  return Promise.each(arguments, function(player, i){
+    return getProfPic(player, i)
+  })
 
 }
 
@@ -57,7 +58,6 @@ function getInsta(player, index) {
     jsonpCallback: "jsonpcallback"
   }) ).then(
     function(data) {
-      console.log(data);
       var followedBy = data.data.counts.followed_by;
       var follows = data.data.counts.follows;
       var ratio = (followedBy / follows).toFixed(4);
@@ -65,11 +65,11 @@ function getInsta(player, index) {
         '<div>followed by: '+followedBy+'</div>'
         + '<div>follows: '+follows+'</div>'
         + '<h1>RATIO: '+ratio+'</h1>'
-        + '<div class="emoji">'+(ratio < 5 ? '🤓' : '😎')+'</div>')
+        + '<div class="emoji">'+(ratio < 0.85 ? '🤓' : '😎')+'</div>')
     }
   ).catch(
     function(err) {
-      console.log(err)
+      console.log("YOU ****ING ****FACE: ", err)
     }
   )
 }
@@ -80,77 +80,119 @@ function getInstas(names) {
   }, {concurrency: 1})
 }
 
-function getMedia(player, index) {
+function getMedia(player, index, maxPages, url) {
+
+  url = url || "https://api.instagram.com/v1/users/"+player.id+"/media/recent?access_token=1578228172.467ede5.04a6ec58145743cc851a2d64b58d9627";
+
   return Promise.resolve( $.ajax({
     method: "GET",
-    url: "https://api.instagram.com/v1/users/"+player.id+"/media/recent?access_token=1578228172.467ede5.04a6ec58145743cc851a2d64b58d9627",
+    url: url,
     dataType: "jsonp",
     jsonpCallback: "jsonpcallback"
   }) ).then(
     function(data) {
-      console.log(data);
-      clear(index);
-      $('#p'+index+'Data').append(
+
+
+      if(player.instas.length == 0)
+      {
+        clear(index);
+        $('#p'+index+'Data').append(
         '<img src="'+data.data[0].images.standard_resolution.url+'" />'
-        + '<div>'+data.data[0].caption.text+'</div>'
-        + '<div>Likes: '+data.data[0].likes.count+'</div>'
-        + '<div>Filter: '+data.data[0].filter+'</div>');
-      player.instas = data.data.map(function(data) {
+        + '<div>'+(data.data[0].caption && data.data[0].caption.text || '')+'</div>'
+        + '<div>Likes: '+(data.data[0].likes.count || 0)+'</div>'
+        + '<div>Filter: '+(data.data[0].filter || '#nofilter')+'</div>');
+      }
+
+      var mappedData = data.data.map(function(data) {
         return {
           url: data.images.standard_resolution.url,
-          caption: data.caption.text,
+          caption: data.caption && data.caption.text || '',
           comments: data.comments.data,
-          filter: data.filter,
-          likes: data.likes.count,
+          filter: data.filter || 'none',
+          likes: data.likes.count || 0,
           tags: data.tags
         };
-      })
+      });
+      player.instas = player.instas.concat(mappedData);
+
+      if(data.pagination && data.pagination.next_url && --maxPages > 0)
+      {
+        return getMedia(player, index, maxPages, data.pagination.next_url);
+      }
+
     }
   ).catch(
     function(err) {
-      console.log(err)
+      console.log(err);
+      throw err;
     }
   )
 }
 
-function getMedias(names) {
-  return Promise.map(arguments, function(name, i) {
-    return getMedia(name, i)
-  }, {concurrency: 1})
+function getMedias(players) {
+  return Promise.each(arguments, function(player, i) {
+    player.instas = [];
+    return getMedia(player, i, 10).then(
+      function(){
+        player.totalLikes = 0;
+        player.instas.forEach(function(insta){
+          player.totalLikes += insta.likes;
+        })
+      }
+    )
+  })
 }
-
-// function getLiked(player, index) {
-//   return Promise.resolve( $.ajax({
-//     method: "GET",
-//     url: "https://api.instagram.com/v1/users/"+player.id+"/media/liked?access_token=1578228172.467ede5.04a6ec58145743cc851a2d64b58d9627",
-//     dataType: "jsonp",
-//     jsonpCallback: "jsonpcallback"
-//   }) ).then(
-//     function(data) {
-//       console.log(data)
-//     }
-//   ).catch(
-//     function(err) {
-//       console.log(err)
-//     }
-//   )
-// }
-
-// function getLikes(names) {
-//   return Promise.map(arguments, function(name, i) {
-//     return getLiked(name, i)
-//   }, {concurrency: 1})
-// }
 
 function clear(index) {
   $('#p'+index+'Data').html('')
 }
 
-// function convertUNIXtime(time){
-//  var date = new Date(time * 1000);
-//  var hours = date.getHours();
-//  var minutes = '0' + date.getMinutes();
-//  var seconds = '0' + date.getSeconds();
+function clearBoth() {
+  clear(0);
+  clear(1);
+}
 
-//  return hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+function displayLikes(players) {
+  return Promise.each(arguments, (function(player, i) {
+    var pData = $('#p'+i+'Data');
+    var pDataWidth = pData.width();
+
+    for ( var l = 0; l < player.totalLikes; l++ ) {
+      pData.append('<img class="absolute" src="http://icons.iconarchive.com/icons/designbolts/free-valentine-heart/16/Heart-icon.png" style="width:10px; height:10px; top:'+(Math.random()*pDataWidth)+'px; left:'+(Math.random()*pDataWidth)+'px"/>')
+    }
+  }) )
+}
+
+function removeLikes() {
+  $('img.absolute').remove()
+}
+
+function switchLikes() {
+  $('img.absolute').each(function(i, like) {
+    if ($(like).parent('#p0Col')) {
+      $(like).css("left", "-=100" );
+    } else {
+      $(like).css("left", "+=100" )
+    }
+  })
+}
+
+// function switch(players) {
+//   return Promise.each(arguments, (function(player,i) {
+//     var p0Col = $('#p0Col');
+//     var p1Col = $('#p1Col');
+
+//     $('#p'+i+'Col img.absolute').remove
+//   }
 // }
+
+function firstBabies(babies) {
+  clearBoth();
+  return Promise.each(arguments, (function(baby, i) {
+    $('#p'+i+'Data').append(
+      '<img src="'+baby.instas[baby.instas.length - 1].url+'" />'
+      + '<div>'+baby.instas[baby.instas.length - 1].caption+'</div>'
+      + '<div>Likes: '+baby.instas[baby.instas.length - 1].likes+'</div>'
+      + '<div>Filter: '+baby.instas[baby.instas.length - 1].filter+'</div>');
+  }))
+}
